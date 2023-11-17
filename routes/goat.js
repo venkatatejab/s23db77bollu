0523cb41fde624df08637b350e6524e3616d5ebc
venkatatejab <143096705+venkatatejab@users.noreply.github.com>
2023-11-17 11:05:12 -0600

    Update goat.js

--- routes/goat.js
+++ routes/goat.js
@@ -9,4 +9,9 @@ const goat_controlers= require('../controllers/goat');
 var router = express.Router();
 /* GET goats */
 router.get('/', goat_controlers.goat_view_all_Page );
+router.get('/detail', goat_controlers.goat_view_one_Page);
+router.get('/create', goat_controlers.goat_create_Page);
+router.get('/update', goat_controlers.goat_update_Page);
+router.get('/delete', goat_controlers.goat_delete_Page);
+
 module.exports = router;

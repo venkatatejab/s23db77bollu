fec3ecb0100f555ca84c4bae40a36ae2014a4535
venkatatejab <143096705+venkatatejab@users.noreply.github.com>
2023-11-29 22:21:30 -0600

    Update goat.js

--- routes/goat.js
+++ routes/goat.js
@@ -7,11 +7,18 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const goat_controlers= require('../controllers/goat');
 var router = express.Router();
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+  }
 /* GET goats */
 router.get('/', goat_controlers.goat_view_all_Page );
-router.get('/detail', goat_controlers.goat_view_one_Page);
-router.get('/create', goat_controlers.goat_create_Page);
-router.get('/update', goat_controlers.goat_update_Page);
-router.get('/delete', goat_controlers.goat_delete_Page);
+router.get('/detail', secured, goat_controlers.goat_view_one_Page);
+router.get('/create', secured, goat_controlers.goat_create_Page);
+router.get('/update', secured, goat_controlers.goat_update_Page);
+router.get('/delete', secured, goat_controlers.goat_delete_Page);
 
 module.exports = router;

4dca28ffe1650ef74e5404c552cb5d45eada0b22
venkatatejab <143096705+venkatatejab@users.noreply.github.com>
2023-11-29 22:21:25 -0600

    Update goat.js

--- models/goat.js
+++ models/goat.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const goatSchema = mongoose.Schema({
-    goat_color:(String),
-    goat_breed:(String),
-    goat_price:(Number)
+goat_color: String,
+goat_breed: {type:String,length:15},
+goat_price: {type:Number,min:1000,max:100000}
 })
-module.exports = mongoose.model("goat", goatSchema)
+module.exports = mongoose.model("goat",goatSchema)
